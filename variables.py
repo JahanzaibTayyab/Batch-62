0cec5cfd1fc4661c064af46677f22deddf46be27
Kamran-it009 <kamran.it009@gmail.com>
2024-08-05 09:00:15 +0500

    Code commit of 2nd class
    Variables, Types of Variables, Escape characters, Format string, String methods

---
+++ variables.py
@@ -0,0 +1,19 @@
+# Data types
+# string: str
+# integer: int
+# float: float
+# Boolean: bool
+name: str # declaration 
+name = "kamran     " # initialization
+age: int = 25
+height:float = 5.8
+married_status: bool = False
+
+print('My name is', name.rstrip(),'.')
+
+
+print(f'I am {age} years old')
+
+
+# print('My height is ', height)
+print('This is \t\tBatch-62 class')

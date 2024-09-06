00c246c1ef4b9fefaad145171866f78749e5d055
Kamran-it009 <kamran.it009@gmail.com>
2024-09-06 09:03:45 +0500

    Update input.py

--- python-learning/25-aug/input.py
+++ python-learning/25-aug/input.py
@@ -1,6 +1,13 @@
+from typing import Union
 # x = 11
 # y = 23
 # print(x + y)
 
-name: str = input('Enter your name')
-print(name)
+# name: str = input('Enter your name')
+# print(name)
+
+f: list[Union[str, int]] = [2, 'apke']
+print(f)
+
+fruits: dict[int, str] = {20 : 'apple'}
+print(fruits)
